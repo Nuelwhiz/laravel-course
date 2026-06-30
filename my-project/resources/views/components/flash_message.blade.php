2d53a4dfdc37ef7b97a1a5f46662e2d3e900338d
Nuelwhiz <emmanuelifeanyi668@gmail.com>
2026-06-30 17:32:21 +0100

    Implement code changes to enhance functionality and improve performance

--- my-project/resources/views/components/flash_message.blade.php
+++ my-project/resources/views/components/flash_message.blade.php
@@ -1,5 +1,6 @@
 @if (session('message'))
-    <div class="bg-green-500 text-white p-4 rounded-lg mb-6 text-center 
+    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" 
+        class="bg-green-500 text-white p-4 rounded-lg mb-6 text-center 
     fixed top-0 px-48 py-3 left-1/2 transform -translate-x-1/2">
         <p>{{ session('message') }}</p>
     </div>
--- my-project/resources/views/listings/create.blade.php
+++ my-project/resources/views/listings/create.blade.php
@@ -25,6 +25,7 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="company"
+                                value={{old("company")}}
                             />
                             @error('company')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -40,6 +41,8 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="title"
                                 placeholder="Example: Senior Laravel Developer"
+                                value={{old("title")}}
+
                             />
                             @error('title')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -57,7 +60,9 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="location"
                                 placeholder="Example: Remote, Boston MA, etc"
-                            />
+                                value={{old("location")}}
+
+                            />\
                             @error('location')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                             @enderror
@@ -71,6 +76,8 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="email"
+                                value={{old("email")}}
+
                             />
                             @error('email')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -88,6 +95,8 @@
                                 type="text"
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="website"
+                                value={{old("website")}}
+
                             />
                             @error('website')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -103,6 +112,8 @@
                                 class="border border-gray-200 rounded p-2 w-full"
                                 name="tags"
                                 placeholder="Example: Laravel, Backend, Postgres, etc"
+                                value={{old("tags")}}
+
                             />
                             @error('tags')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -132,6 +143,8 @@
                                 name="discription"
                                 rows="10"
                                 placeholder="Include tasks, requirements, salary, etc"
+                                {{old("discription")}}
+
                             ></textarea>
                             @error('discription')
                             <p class="text-red-500 text-xs mt-1">{{$message}}</p>
